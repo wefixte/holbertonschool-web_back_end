8682874a8f3019a107c903df709a618d03adb7bd
wefixte <cassy.pernelle@gmail.com>
2024-04-24 10:05:30 +0200

    Add T5

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+""" Returns sum of a list of floats as a float"""
+
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """ Returns the sum of a list of floats as a float
+    Args:
+        input_list: list of floats
+    """
+    return sum(input_list)
