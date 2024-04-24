b99fa9156fe50958e3c3cc50f8d3b04759643501
wefixte <cassy.pernelle@gmail.com>
2024-04-24 09:55:29 +0200

    Add T1

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" Concatenate 2 strings and return a concatenate string """
+
+
+def concat(str1: str, str2: str) -> str:
+    """ Concatenate 2 strings and return the result
+    Args:
+        str1: First string
+         str2: Second string
+    """
+    return str1 + str2
