75d20eadddddd771e723033e9337aab39fdadb53
wefixte <cassy.pernelle@gmail.com>
2024-04-24 09:54:01 +0200

    add T0

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" Returns the sum of 2 floats """
+
+def add(a: float, b: float) -> float:
+    """ Returns the sum of a and b
+    Args:
+        a: First float
+        b: Second float
+    """
+    return a + b
