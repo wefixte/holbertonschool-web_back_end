bf3a0bb61a466eeb76537bf5b2c77f56a739d44a
wefixte <cassy.pernelle@gmail.com>
2024-04-24 09:58:30 +0200

    Add T2

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" Returns the floor of a float n """
+
+
+def floor(n: float) -> int:
+    """ Returns the floor of a float
+    Args
+        n: float to be floored
+    """
+    return int(n)
