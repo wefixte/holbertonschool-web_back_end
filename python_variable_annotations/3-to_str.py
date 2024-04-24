2f28e388cf8d7181f104101f4216343fef08fb03
wefixte <cassy.pernelle@gmail.com>
2024-04-24 10:00:27 +0200

    Add T3

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" Return the string representation of a float """
+
+
+def to_str(n: float) -> str:
+    """ Return the string representation of a float
+    Args:
+        n: float to convert to string
+    """
+    return str(n)
